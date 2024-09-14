23a46515770394ebefda540f64999ace888453c8
GSM429 <gavmontross@gmail.com>
2024-09-14 15:02:39 -0400

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {Navbar} from './components/navbar'
-import './App.css'
-import { Auth } from './pages/auth'
-import { Home } from './pages/home'
-import { Movie } from './pages/movie'
-import { TvShow } from './pages/tvshow'
-import { Rated } from './pages/rated'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/navbar';
+import './App.css';
+import { Auth } from './pages/auth';
+import { Home } from './pages/home';
+import { Movie } from './pages/movie';
+import { TvShow } from './pages/tvshow';
+import { Rated } from './pages/rated';
 
 function App() {
-
-
   return (
     <div>
-      <Router> 
+      <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/auth" element={<Auth/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
           <Route path="/rated" element={<Rated />} />
           <Route path="/movie/:id" element={<Movie />} />
           <Route path="/tvshow/:id" element={<TvShow />} />
-          </Routes>
+        </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
